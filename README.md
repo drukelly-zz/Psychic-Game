f8b724d5557e9de8e7b30deffe0e4dd04c47963a
Dru Kelly <drukelly@gmail.com>
2019-03-10 05:43:55 -0700

    wip

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Psychic Game
 
 ## Rules
-Try to guess the letter in less than 9 tries. If you guess correctly, you get a point! If you don't, CPU will get a point. First player to get to 10 points win!
+Try to guess the letter in less than 9 tries. If you guess correctly, you get a point! If you don't, CPU gets a point. First player to get to 10 points win!
 
 ## Details
 - Game will restart after guessing the letter correctly
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,24 @@
 const user = {
-  "score" : 0,
-  "chances" : 9
+  "chances" : 9,
+  "score" : 0
 };
+const cpu = { "score" : 0 };
 const guesses = [];
 const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const lettersGuessed = document.getElementsByClassName("lettersGuessed")[0];
+const modal = document.getElementsByClassName("modal")[0];
+const overlay = document.getElementsByClassName("overlay")[0];
 
 document.onkeyup = (event) => {
-  letterChecker(event.key);
+  isLetter(event.key);
   console.log(user.score, user.chances);
 }
 
-const letterChecker = (letter) => {
+const isLetter = (letter) => {
   if (letters.indexOf(letter[0]) !== -1) {
     guesses.push(letter);
     lettersGuessed.append(`${guesses.pop()}, `);
+    modalBegone(modal, overlay);
     // console.log(letter);
   } else {
     console.log("not a letter!");
@@ -23,4 +27,17 @@ const letterChecker = (letter) => {
 
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+const randomLetter = (array) => {
+  console.log(array[getRandomInt(array.length)]);
+}
+
+const incrementScore = (player) => {
+  ++player;
+}
+
+const modalBegone = (modal, overlay) => {
+  overlay.remove();
+  modal.remove();
 }
