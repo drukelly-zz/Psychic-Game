aece0606fdbb3ee2fefda4bd57b3eeca606ea380
Dru Kelly <drukelly@gmail.com>
2019-03-12 01:45:53 -0700

    saving wip

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,13 +1,28 @@
+// Functions
 const isLetter = (letter, array) => {
   if (array.indexOf(letter[0]) !== -1) {
-    console.log(letter);
+    // --user["chances"];
+    return true;
   } else {
     // alert(`You typed in ${event.key}. Please type in a valid letter.`);
     console.log(`You typed in ${event.key}. Please type in a valid letter.`);
   }
 }
+const generateLetter = () => {
+  var charToGuess = letters[getRandomInt(letters.length)];
+  console.log(`the winning key is => ${charToGuess}`);
+}
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
+}
+const noRepeatedEntries = (letter, array) => {
+  if (array.indexOf(letter[0]) !== -1) {
+    console.log("repeated entry");
+  } else {
+    guesses.push(letter);
+    lettersGuessed.innerHTML += `<span class="letter">${guesses[guesses.length-1]}</span>`;
+    console.log(guesses);
+  }
 }
 const updateScores = () => {
   remainingGuesses.innerText = user["chances"];
@@ -15,35 +30,46 @@ const updateScores = () => {
   cpuScore.innerText = cpu["score"];
 }
 
+// Objects
 let cpu = {
   "score" : 0
 }
 let user = {
-  "chances" : 9,
+  "chances" : 3,
   "score" : 0
 }
-
+// Arrays
 const guesses = [];
 const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const generatedCharacter = letters[getRandomInt(letters.length)];
 
+// Selectors
 const remainingGuesses = document.getElementsByClassName("guesses")[0];
 const userScore = document.getElementsByClassName("user")[0];
 const cpuScore = document.getElementsByClassName("cpu")[0];
+const lettersGuessed = document.getElementsByClassName("user-guesses")[0];
 
-document.onkeypress = (event) => {
-  console.log(`the winning key is => ${generatedCharacter}`);
-  let guess = event.key;
-  if (isLetter(guess, letters)) {
-    return true;
-  } else {
-    (guess === generatedCharacter) ? ++user["score"] : --user["chances"];
-    if (user["chances"] === 0) {
-      ++cpu["score"];
+// Events
+// onload
+document.addEventListener("DOMContentLoaded", () => {
+  generateLetter();
+  updateScores();
+  // onkeypress
+  document.onkeypress = (event) => {
+    let guess = event.key;
+    if (isLetter(guess, letters) && noRepeatedEntries(guess, guesses)) {
+      if (guess === charToGuess) {
+        console.log("win!")
+        // ++user["score"];
+        // guesses.length = 0;
+        // console.log(user);
+      }
+      if (user["chances"] === 0) {
+        console.log("lose")
+        // ++cpu["score"];
+        // user["chances"] = 3;
+        // console.log(cpu);
+      }
+      updateScores();
     }
   }
-  updateScores();
-}
-
-// onload
-updateScores();
+});
